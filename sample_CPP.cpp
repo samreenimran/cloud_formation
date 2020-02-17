a724fd68fc4177409498c50919588fcd81d3ae9f
Samreen Imran Bagawan <EE211324@sasken.com>
2020-02-17 14:12:48 +0530

    sample_cpp_code

---
+++ sample_CPP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string e_id;
+    cout<<"Hello World"<<endl;
+    cout<<"Hi this is a sample code deploying through cloud formation"<<endl;
+    cout<<"Enter your employee id"<<endl;
+    cin>>e_id;
+    if(e_id == "ee211324")
+        cout<<"Hi samreen"<<endl;
+
+    return 0;
+}
